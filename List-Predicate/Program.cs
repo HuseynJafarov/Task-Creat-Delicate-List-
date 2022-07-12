c1b7fc97d2f04543cb2723e089352822e7728b27
HuseynJafarov <huseynnj@code.edu.az>
2022-07-13 02:37:28 +0400

    checkName

---
+++ List-Predicate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_Predicate
+{
+    class Program
+    {
+        public delegate bool CheckString(string checkName);
+
+        static void Main(string[] args)
+        {
+            List<string> check = new List<string>();
+            check.Add("Huseyn");
+            check.Add("Maqa");
+            check.Add("Azer");
+            check.Add("Eli");
+            check.Add("Cavid");
+            check.Add("Orxan");
+
+            check.FindAll(CheckName);
+
+            
+
+        }
+
+        public static bool CheckName(string name)
+        {
+            if(name == "Huseyn")
+            {
+                Console.WriteLine(name);
+            }
+            return true;
+        }
+
+    }
+}
